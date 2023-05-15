aa25acec2efbb56629ec429f899761ae7810179b
Rub Dopstoyesky <rub.dopstoyesky1251@gmail.com>
2023-05-15 00:37:14 -0600

    se sube con array

---
+++ main.js
@@ -0,0 +1,102 @@
+function calculadora(num1, num2, operacion) {
+    function suma(num1, num2){
+        return num1 + num2;
+    }
+    
+    function resta(num1, num2){
+        return num1 - num2;
+    }
+    
+    function multiplicacion(num1, num2){
+        return num1 * num2;
+    }
+    
+    function division(num1, num2){
+        return num1 / num2;
+    }
+    switch (operacion) {
+        case "+":
+            return suma(num1,num2);
+        case "-":
+            return resta(num1,num2);
+        case "*":
+            return multiplicacion(num1,num2);
+        case "/":
+            if (num2 === 0) {
+                return "No se puede dividir entre cero";
+            } else {
+                return division(num1,num2);
+            }
+        default:
+            return "Opcion invalida";
+    }
+}
+
+let salir = false;
+
+while (!salir) {
+    let numero1 = null;
+    let numero2 = null;
+
+    while (
+        numero1 === null ||
+        isNaN(numero1) ||
+        numero1 === "" ||
+        /^\s*$/.test(numero1)
+    ) {
+        numero1 = prompt(
+            "Ingrese el primer número o presione la tecla 'Esc' para salir"
+        );
+        if (numero1 === null || numero1.toLowerCase() === "esc") {
+            salir = true;
+            break;
+        }
+    }
+
+    if (salir) {
+        break;
+    }
+
+    while (
+        numero2 === null ||
+        isNaN(numero2) ||
+        numero2 === "" ||
+        /^\s*$/.test(numero2)
+    ) {
+        numero2 = prompt(
+            "Ingrese el segundo número o presione la tecla 'Esc' para salir"
+        );
+        if (numero2 === null || numero2.toLowerCase() === "esc") {
+            salir = true;
+            break;
+        }
+    }
+
+    if (salir) {
+        break;
+    }
+
+    let operacion = prompt(
+        "Ingrese la operación (+, -, *, /) o presione la tecla 'Esc' para salir"
+    );
+
+    if (operacion === null || operacion.toLowerCase() === "esc") {
+        salir = true;
+        break;
+    }
+
+    let resultados = [];
+    let resultado = calculadora(+numero1, +numero2, operacion);
+    if (isNaN(resultado)) {
+        alert("Ingrese valores válidos");
+    } else {
+        alert(`El resultado es ${resultado}`);
+        resultados.push(resultado); // Agregar el resultado al nuevo arreglo
+        console.log("Resultados:", resultados); // Imprimir el arreglo completo en la consola
+    }
+}
+
+if (salir) {
+    alert("Saliendo de la calculadora");
+    alert(`Resultados: ${resultados.join(", ")}`);
+} 
